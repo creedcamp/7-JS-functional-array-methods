ac16eefb3c0ad3c3ce20c26375a2d5443974158f
Creed Camp <creedomed@gmail.com>
2020-09-04 07:41:55 -0700

    unfortunately, turning in....but I'll be back

---
+++ main.js
@@ -0,0 +1,12 @@
+let newString = ""
+for (let i = 0; i <= item.length; i++) const arr = [item]
+const reducer = (currentValue, currentIndex) => (
+  (newString + currentValue) / item.length, currentIndex
+)
+
+index.html.querySelector("#answer1")
+const answer1 = {
+  querySelector: function (reducer) {
+    console.log(newString)
+  },
+}
