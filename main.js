794358ff69f2b3d0bc59878a7fc3d98535e4c91f
Creed Camp <creedomed@gmail.com>
2020-09-08 09:04:49 -0700

    turning in

--- main.js
+++ main.js
@@ -1,12 +1,38 @@
-let newString = ""
-for (let i = 0; i <= item.length; i++) const arr = [item]
-const reducer = (currentValue, currentIndex) => (
-  (newString + currentValue) / item.length, currentIndex
-)
+//question 1 answer:
+//const arr = items.map(function (item) {
+//  return item.price
+//})
+//const avgPrice = arr.reduce((a, b) => a + b) / arr.length
+//document.querySelector(`#answer1`).innerHTML = `The way to find average price is ${avgPrice}`
 
-index.html.querySelector("#answer1")
-const answer1 = {
-  querySelector: function (reducer) {
-    console.log(newString)
-  },
-}
+//console.log(avgPrice)
+
+//question 2 answer:
+//const arr = items.map(function (item) {
+//  return item.price
+//})
+//const arr = items.filter(function (item) {
+//  if (item.price >= 14 && item.price <= 18) {
+//    return true
+//  } else {
+//    return false
+//  }
+//})
+//const costBetweenAnswer = arr.map(function(item) {
+//  return `<li>${item.title}</li>`
+//}).join(' ')
+//document.querySelector(`#answer2`).innerHTML = `$(costBetweenAnswer)`
+
+//question 3 answer:
+
+//const gbp = items.filter(function (item) {
+//  if (item.currency_code === "GBP") {
+//    return true
+//  } else {
+//    return false
+//  }
+//})
+//const gbpAnswer = gbp.map(function (item) {
+//  return `${item.title}" , "${item.price}`
+//})
+//document.querySelector("answer3").innerHTML = `$(gpbAnswer)`
