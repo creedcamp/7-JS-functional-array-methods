a73ccb3c0bab2598968e86984f9dd22a9f0e6e31
Creed Camp <creedomed@gmail.com>
2020-09-18 11:54:45 -0700

    saving the stuff

--- main.js
+++ main.js
@@ -1,38 +1,49 @@
 //question 1 answer:
-//const arr = items.map(function (item) {
-//  return item.price
-//})
-//const avgPrice = arr.reduce((a, b) => a + b) / arr.length
-//document.querySelector(`#answer1`).innerHTML = `The way to find average price is ${avgPrice}`
+const mapPrice = items.map(function (item) {
+  return (item + item.price) / item.price.length
+})
 
-//console.log(avgPrice)
+// const avgPrice = items.reduce(function (a, b) {
+//   return (a + b.price)
+// }) / items.price.length
+
+// const avgPrice = mapPrice.reduce((item) {
+//   return (item + mapPrice) / mapPrice.length
+// })
+document.getElementById("#answer1").innerHTML = avgPrice
+
+console.log(avgPrice)
 
 //question 2 answer:
-//const arr = items.map(function (item) {
-//  return item.price
-//})
-//const arr = items.filter(function (item) {
-//  if (item.price >= 14 && item.price <= 18) {
-//    return true
-//  } else {
-//    return false
-//  }
-//})
-//const costBetweenAnswer = arr.map(function(item) {
-//  return `<li>${item.title}</li>`
-//}).join(' ')
-//document.querySelector(`#answer2`).innerHTML = `$(costBetweenAnswer)`
+// const arr2 = items.map(function (item) {
+//   return item.price
+// })
+const arr = items.filter(function (item) {
+  if (item.price >= 14 && item.price <= 18) {
+    console.log(item.price)
+    return item.price
+  } else {
+    return false
+  }
+})
+const costBetweenAnswer = arr
+  .map(function (item) {
+    console.log(item)
+    return `<li>${item.title}</li>`
+  })
+  .join(" ")
+document.querySelector(`#answer2`).innerHTML = $(costBetweenAnswer)
 
 //question 3 answer:
 
-//const gbp = items.filter(function (item) {
-//  if (item.currency_code === "GBP") {
-//    return true
-//  } else {
-//    return false
-//  }
-//})
-//const gbpAnswer = gbp.map(function (item) {
-//  return `${item.title}" , "${item.price}`
-//})
-//document.querySelector("answer3").innerHTML = `$(gpbAnswer)`
+const gbp = items.filter(function (item) {
+  if (item.currency_code === "GBP") {
+    return true
+  } else {
+    return false
+  }
+})
+const gbpAnswer = gbp.map(function (item) {
+  return `${item.title}" , "${item.price}`
+})
+document.querySelector("answer3").innerHTML = `$(gpbAnswer)`
